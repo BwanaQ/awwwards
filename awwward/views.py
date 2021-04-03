cf1ab3865d2bf345a5d4aad0bf86dea975a53508
BwanaQ <thunjawax@gmail.com>
2021-04-03 14:31:11 +0300

    add dummy content to home template

--- awwward/views.py
+++ awwward/views.py
@@ -1,38 +1,44 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
+import datetime
+from django.utils.timezone import utc
+
 
 projects = [
     {
-        'author': 'BwanaQ',
+        'creator': 'BwanaQ',
         'title': 'Dummy Content',
         'image': 'Blog Post 1',
         'description': 'First post content',
-        'url': 'August 27, 2020',
+        'link': 'August 27, 2020',
         'timestamp': 'August 27, 2020'
     },
     {
-        'author': 'Sally Koki',
+        'creator': 'Sally Koki',
         'title': 'Dummy Content',
         'image': 'Blog Post 2',
         'description': 'Second post content',
-        'url': 'November 27, 2021',
+        'link': 'November 27, 2021',
         'timestamp': 'August 27, 2020'
 
     },
     {
-        'author': 'Loise Hunja',
+        'creator': 'Loise Hunja',
         'title': 'Dummy Content',
         'image': 'Blog Post 3',
         'description': 'Third post content',
-        'url': 'March 2, 2021'
+        'link': 'March 2, 2021',
         'timestamp': 'August 27, 2020'
     }
 ]
 
 
 def home(request):
+    # Create your views here.
+    now = datetime.datetime.utcnow().replace(tzinfo=utc)
+
     context = {
-        'projects': projects
+        'projects': projects,
+        'now': now
     }
     return render(request, 'awwward/home.html', context)
--- core/settings.py
+++ core/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # 3rd party apps
+    'cloudinary',
+    'cloudinary_storage',
+    'rest_framework',
 
     # my apps
     'awwward',
@@ -120,5 +124,20 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': os.environ.get('CLOUD_NAME'),
+    'API_KEY': os.environ.get('API_KEY'),
+    'API_SECRET': os.environ.get('API_SECRET'),
+}
+
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
